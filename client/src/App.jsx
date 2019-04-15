68cfe89851e28aecd19285e01ab780c10b01e22e
kamekazz <taveras78@gmail.com>
2019-04-15 02:21:05 -0700

    boostrap

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+import Landing from './components/layouts/Landing';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <Landing />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
